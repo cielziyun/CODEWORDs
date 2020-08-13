9d023f3bb5b5f15033efceac4ec000fab313f3c3
cielziyun <68975607+cielziyun@users.noreply.github.com>
2020-08-13 13:26:17 +0800

    Add files via upload

---
+++ SKO/WEEK 4/week_4_text_spin/week_4_text_spin.js
@@ -0,0 +1,33 @@
+function preload(){
+  font = loadFont('data/QanelasSoftDEMO-UltraLight.otf');
+  }
+var spin=0; circleX=0; rectY=0;
+function setup() {
+createCanvas(600,600);
+background(250,250,100);
+textAlign(LEFT);
+}
+
+function draw() {
+ 
+ fill(0,200,200,30);
+ noStroke();
+ ellipse(circleX, 300, 20, 20);
+ circleX = circleX+1; 
+ 
+ fill(200,0,200,30);
+ noStroke();
+ rect(200, rectY, 30, 30);
+ rectY = rectY+1; 
+  
+ rotate(spin);
+ fill(100, 100, 0);
+ textSize(16);
+ textFont (font);
+ textFont(font,16);
+ text('hello world, my name is ciellit',300,200);
+ spin=spin-10;
+ 
+
+
+}
